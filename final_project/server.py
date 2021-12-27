5153239d914098e568bde68e65a514204358e365
ninekind <jazzlove2594@gmail.com>
2021-12-27 23:59:34 +0000

    m

--- final_project/server.py
+++ final_project/server.py
@@ -10,19 +10,20 @@ app = Flask("Web Translator")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    return machinetranslation.translator.englishToFrench(textToTranslate)
 
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+    return machinetranslation.translator.frenchToEnglish(textToTranslate)
 
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
